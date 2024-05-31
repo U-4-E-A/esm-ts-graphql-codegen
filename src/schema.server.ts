34e4437ec3b0ea4dff2dd4195cde0f9d1b644949
A Keys <alasdairkeys@outlook.com>
2024-05-31 17:38:43 +0100

    inline example added

--- src/schema.server.ts
+++ src/schema.server.ts
@@ -2,15 +2,15 @@ import { mergeTypeDefs } from "@graphql-tools/merge";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { fileURLToPath } from "url";
-import path, { dirname } from "path";
+import path from "path";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
 const typeDefs = mergeTypeDefs(
-  loadFilesSync(`./graphql/schema/**/*`)
+  // loadFilesSync(`./graphql/schema/**/*`)
   // loadFilesSync(`${__dirname}/graphql/schema/**/*`)
-  // loadFilesSync(`file:///${__dirname}/graphql/schema/enum/enum.ts`)
+  loadFilesSync(`file:///${__dirname}/graphql/schema/**/*`)
 );
 
 const schema = makeExecutableSchema({
