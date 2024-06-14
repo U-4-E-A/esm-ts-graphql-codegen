e71747975c8aaa3017a49e2991abdcede6e4ec84
A Keys <alasdairkeys@outlook.com>
2024-06-14 23:47:51 +0100

    Ongoing

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,12 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
+import { pathToFileURL } from "url";
+
+// console.log(__dirname);
+// console.log(`${pathToFileURL(__dirname).href}/src/schema.server.ts`);
 
 const config: CodegenConfig = {
   // schema: `./src/schema.server.ts`,
-  schema: `file:///${__dirname}/src/schema.server.ts`,
-  // schema: `file://C:/dev/esm-ts-graphql-codegen/src/schema.server.ts`,
-  // emitLegacyCommonJSImports: false,
-  // require: ["ts-node/register/transpile-only", "tsconfig-paths/register.js"],
-  // generates: {
-  //   "./src/types/schema.json": {
-  //     plugins: ["introspection"],
-  //   },
-  // },
+  schema: `${pathToFileURL(__dirname).href}/src/schema.server.ts`,
   require: ["tsx/esm", "tsconfig-paths/register.js"],
   generates: {
     [`./src/types/schema.json`]: {
--- src/schema.server.ts
+++ src/schema.server.ts
@@ -3,18 +3,44 @@ import { loadFilesSync } from "@graphql-tools/load-files";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { fileURLToPath } from "url";
 import path from "path";
+import { pathToFileURL } from "url";
 
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
+// const typesArray = loadFilesSync(
+//   `${pathToFileURL(__dirname).href}/graphql/schema/**/*`
+// );
+// console.log("typesArray : ", typesArray);
+
+// const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+// const __dirname = path.dirname(__filename); // get the name of the directory
+// // console.log("__filename");
+// const typeDefs = mergeTypeDefs(
+//   // loadFilesSync(`./src/graphql/schema/**/*`)
+//   // loadFilesSync(`${__dirname}/graphql/schema/**/*`)
+//   // loadFilesSync(`${pathToFileURL(__dirname).href}/graphql/schema/**/*`)
+//   // loadFilesSync(`file://${__dirname}/graphql/schema/**/*`)
+//   loadFilesSync(typesArray)
+// );
+
+// const schema = makeExecutableSchema({
+//   typeDefs,
+// });
+
+// export default schema;
 
 const typeDefs = mergeTypeDefs(
-  loadFilesSync(`./src/graphql/schema/**/*`)
-  // loadFilesSync(`${__dirname}/graphql/schema/**/*`)
-  // loadFilesSync(`file:///${__dirname}/graphql/schema/**/*`)
+  loadFilesSync([
+    path.join(__dirname, "../graphql/schema/**/*"),
+    // path.join(__dirname, '../../../../../graphql/src/schema/**/*')
+  ])
+);
+// @ts-ignore
+const resolvers = mergeResolvers(
+  loadFilesSync(path.join(__dirname, "../graphql/resolvers"))
 );
 
 const schema = makeExecutableSchema({
   typeDefs,
+  resolvers,
 });
 
 export default schema;
