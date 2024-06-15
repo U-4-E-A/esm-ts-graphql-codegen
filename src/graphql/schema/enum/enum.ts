cbbf27a3093ca2ca403b7e3a3b227d968a077880
A Keys <alasdairkeys@outlook.com>
2024-06-15 23:10:17 +0100

    Ongoing

--- src/graphql/schema/enum/enum.ts
+++ src/graphql/schema/enum/enum.ts
@@ -1,36 +1,6 @@
-import gql from "graphql-tag";
-
-// // test export
-// export default `{
-//     hero {
-//       name
-//       appearsIn
-//     }
-//   }`;
-
 export default `
   enum hero {
     name
     appearsIn
   }
 `;
-
-// export const typeDefs = gql`
-//   enum hero {
-//     name
-//     appearsIn
-//   }
-
-//   # extend type Query {
-//   #   isLoggedIn_____________: User!
-//   #   cartItems: [ID!]!
-//   # }
-
-//   # extend type Launch {
-//   #   isInCart: Boolean!
-//   # }
-
-//   # extend type Mutation {
-//   #   addOrRemoveFromCart(id: ID!): [ID!]!
-//   # }
-// `;
